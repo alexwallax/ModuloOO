c61337a0ab733c6c91dd17c0329023c26daafac3
Ale Wallace <alexwallax@gmail.com>
2024-09-04 09:22:17 -0300

    Orientação a objeto 02

---
+++ class03.py
@@ -0,0 +1,16 @@
+class Animal:
+
+    def __init__(self, nome):
+        self.nome = nome
+
+        variavel = "valor"
+        print(variavel)
+
+    def comendo(self, alimento):
+        return f'{self.nome} está comendo {alimento}'
+
+
+
+leao = Animal(nome='Leão')
+print(leao.nome)
+print(leao.comendo('Maçã'))
